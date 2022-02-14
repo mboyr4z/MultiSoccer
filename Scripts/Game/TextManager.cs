39bec08149a90270d7cdbab2207b2d65729c0e1b
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2022-02-15 02:37:39 +0300

    Script Önceliğinde Random Join daha geç çalışacak

--- Scripts/Game/TextManager.cs
+++ Scripts/Game/TextManager.cs
@@ -9,6 +9,7 @@ public class TextManager : Singleton<TextManager>
     private void Start()
     {
         alan = GetComponent<Text>();
+        print("alan var " + alan.ToString());
     }
     public void Ekle(string yazi)
     {
