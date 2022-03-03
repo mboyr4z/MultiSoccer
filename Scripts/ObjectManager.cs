3ce97ac09e07724a1bef31c9ccd27e1a1c07fdc8
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2022-03-03 21:55:52 +0300

    ObjectManager Oluşturuldu ve UI elementlerinin isimleri düzenlendi

---
+++ Scripts/ObjectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectManager : Singleton<ObjectManager>
+{
+    public TMP_InputField Input_RoomName;
+    public TMP_InputField Input_MaxPlayer;
+
+    public TMP_Text Text_Error;
+    public TMP_Text Text_RoomName;
+
+    public Transform RoomListContent;
+    public Transform PlayerListContent;
+
+    public GameObject Button_StartGame;
+
+    public GameObject pre_RoomListItemPrefab;
+    public GameObject pre_PlayerListItemPrefab;
+}
