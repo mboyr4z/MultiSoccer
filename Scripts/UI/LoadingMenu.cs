fb8324544099d5e2cfcd80b9eca20d470f07f2e5
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2022-03-05 17:07:20 +0300

    Loading ekranı top döndürme tamam ve loading yazısının noktaları tamam

--- Scripts/UI/LoadingMenu.cs
+++ Scripts/UI/LoadingMenu.cs
@@ -12,10 +12,34 @@ public class LoadingMenu : MonoBehaviour
 
     [SerializeField] private GameObject SpinningBall;
 
+    private int textCtr = 0;
+
 
     private void OnEnable()
     {
         SpinningBallStyle.Spin(SpinningBall);       // ilgili döndğrme stili ile topu döndür
+        InvokeRepeating("LoadingTextRepeater",0.0f,0.4f);
+    }
+
+    private void LoadingTextRepeater()
+    {
+
+        textCtr++;
+        switch (textCtr % 3)
+        {
+            case 0:
+                TMP_Loading.text = "Loading.";
+                break;
+            case 1:
+                TMP_Loading.text = "Loading..";
+                break;
+            case 2:
+                TMP_Loading.text = "Loading...";
+                break;
+            default:
+                TMP_Loading.text = "Loading.";
+                break;
+        }
     }
     private void OnDisable()
     {
