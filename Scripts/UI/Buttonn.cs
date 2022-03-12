53e3c8874858889c191f9478dfee996dcecb999c
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2022-03-12 03:38:06 +0300

    Bazı Butonların Onclicki kapatılır ve buton scripti eklenir

--- Scripts/UI/Buttonn.cs
+++ Scripts/UI/Buttonn.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class Buttonn : MonoBehaviour
 {
     [SerializeField] private string menuName;
-    private void Start()
+
+    private void OnEnable()
     {
+        print(this.name + " butonu aktif oldu");
         GetComponent<Button>().onClick.AddListener(OpenMenu);
     }
 
     private void OpenMenu()
     {
+        print(this.name + " butonuna tıklandı");
         MenuManager.Instance.OpenMenu(menuName);
     }
 }
