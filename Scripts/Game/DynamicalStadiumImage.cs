803e756936bec11a58bac2ad7672f9410a64e16e
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2022-03-24 15:36:51 +0300

    StadiumBackground Image not fixed now

---
+++ Scripts/Game/DynamicalStadiumImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicalStadiumImage : MonoBehaviour
+{
+    private int width, height;
+
+    const float Y = 1.54f;
+
+    private float X;
+
+
+
+
+
+    void Start()
+    {
+
+        width = Screen.width;
+        height = Screen.height;;
+
+        X = (Y / 2) / height * width;
+        transform.localScale = new Vector3(X,Y,0);
+    }
+
+    
+}
